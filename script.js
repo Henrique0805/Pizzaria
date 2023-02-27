3088fee0716acb622f2aea5a2c7b1af9c06be846
henrique.amorim <henrique.amorim@zanthus.com.br>
2023-02-27 19:12:39 -0300

    Adicionando conteúdo das pizzas e modal de compra.

--- script.js
+++ script.js
@@ -8,5 +8,22 @@ const selectorAll = (e)=> {
 pizzaJson.map((item, index)=>{
     let pizzaItem = selector('.models .pizza-item').cloneNode(true);
 
+    pizzaItem.querySelector('.pizza-item--img img').src = item.img;
+    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
+    pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
+    pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
+    pizzaItem.querySelector('a').addEventListener('click', (e)=>{
+        e.preventDefault();
+
+        console.log("Clicou na pizza!");
+
+        selector('.pizzaWindowArea').style.opacity = 0;
+        selector('.pizzaWindowArea').style.display = 'flex';
+        setTimeout(() => {
+            selector('.pizzaWindowArea').style.opacity = 1;
+        }, 200);
+        
+    });
+
     selector('.pizza-area').append(pizzaItem);
 });
