248697b6784376fd1308726867cfb20c666e40e6
henrique.amorim <henrique.amorim@zanthus.com.br>
2023-03-01 19:32:44 -0300

    Inserindo informações no modal e adicionando ação nos botões de cancelar

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let modalQtd = 1;
+
 const selector = (e)=> {
     return document.querySelector(e);
 }
@@ -5,17 +7,33 @@ const selectorAll = (e)=> {
     return document.querySelectorAll(e);
 }
 
+//Listagem das pizzas
 pizzaJson.map((item, index)=>{
     let pizzaItem = selector('.models .pizza-item').cloneNode(true);
 
+    pizzaItem.setAttribute('data-key', index);
     pizzaItem.querySelector('.pizza-item--img img').src = item.img;
     pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
     pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
     pizzaItem.querySelector('a').addEventListener('click', (e)=>{
         e.preventDefault();
+        let key = e.target.closest('.pizza-item').getAttribute('data-key');
+        modalQtd = 1;
+    
+        selector('.pizzaBig img').src = pizzaJson[key].img;
+        selector('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
+        selector('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
+        selector('.pizzaInfo--actualPrice').innerHTML = `R$ ${pizzaJson[key].price.toFixed(2)}`;
+        selector('.pizzaInfo--size.selected').classList.remove('selected');
+        selectorAll('.pizzaInfo--size').forEach((size, sizeIndex)=>{
+            if(sizeIndex == 2) {
+                size.classList.add('selected');
+            }
+            size.querySelector('span').innerHTML = pizzaJson[key].sizes[sizeIndex];
+        });
 
-        console.log("Clicou na pizza!");
+        selector('.pizzaInfo--qt').innerHTML = modalQtd;
 
         selector('.pizzaWindowArea').style.opacity = 0;
         selector('.pizzaWindowArea').style.display = 'flex';
@@ -26,4 +44,15 @@ pizzaJson.map((item, index)=>{
     });
 
     selector('.pizza-area').append(pizzaItem);
+});
+
+// Eventos do Modal
+function closeModal() {
+    selector('.pizzaWindowArea').style.opacity = 0;
+    setTimeout(() => {
+        selector('.pizzaWindowArea').style.display = 'none';
+    }, 500);
+}
+selectorAll('.pizzaInfo--cancelMobileButton, .pizzaInfo--cancelButton').forEach((item)=>{
+    item.addEventListener('click', closeModal)
 });
