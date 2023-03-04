44d12df821a39ef863b883c0e111783df77fb645
henrique.amorim <henrique.amorim@zanthus.com.br>
2023-03-03 23:08:16 -0300

    Implementando a parte mobile.

--- script.js
+++ script.js
@@ -96,13 +96,61 @@ selector('.pizzaInfo--addButton').addEventListener('click', ()=>{
 });
 
 function updateCart() {
+    selector('.menu-openner span').innerHTML = cart.length;
+
     if(cart.length > 0) {
         selector('aside').classList.add('show');
+        selector('.cart').innerHTML = '';
+
+        let subtotal = 0;
+        let desconto = 0;
+        let total = 0;
+
         for(let i in cart) {
             let pizzaItem = pizzaJson.find((item)=>{
                 return item.id == cart[i].id;
             });
+            subtotal += pizzaItem.price * cart[i].qtd;
+
+            let cartItem = selector('.models .cart--item').cloneNode(true);
+            let pizzaSizeName = '';
+            switch(cart[i].size) {
+                case 0:
+                    pizzaSizeName = 'P';
+                    break;
+                case 1:
+                    pizzaSizeName = 'M';
+                    break;
+                case 2:
+                    pizzaSizeName = 'G';
+                    break;
+            }
+            let pizzaName = `${pizzaItem.name} (${pizzaSizeName})`;
+            cartItem.querySelector('img').src = pizzaItem.img;
+            cartItem.querySelector('.cart--item-nome').innerHTML = pizzaName;
+            cartItem.querySelector('.cart--item--qt').innerHTML = cart[i].qtd;
+            cartItem.querySelector('.cart--item-qtmenos').addEventListener('click', () =>{
+                if(cart[i].qtd > 1) {
+                    cart[i].qtd--;
+                } else {
+                    cart.splice(i, 1);
+                }
+                updateCart();
+            });
+            cartItem.querySelector('.cart--item-qtmais').addEventListener('click', () =>{
+                cart[i].qtd++;
+                updateCart();
+            });
+
+            selector('.cart').append(cartItem);
         }
+        desconto = subtotal * 0.1;
+        total = subtotal - desconto;
+
+        selector('.subtotal span:last-child').innerHTML = `R$ ${subtotal.toFixed(2)}`;
+        selector('.desconto span:last-child').innerHTML = `R$ ${desconto.toFixed(2)}`;
+        selector('.total span:last-child').innerHTML = `R$ ${total.toFixed(2)}`;
+
     }else{
         selector('aside').classList.remove('show');
     }
